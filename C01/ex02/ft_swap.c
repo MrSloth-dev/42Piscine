bafbd50dcde3279e517c284eaca751eab9e7ed15
Mr_slothdev <joao8barbosa@gmail.com>
2024-02-01 23:41:22 +0000

    added main to c01/ex02

--- C01/ex02/ft_swap.c
+++ C01/ex02/ft_swap.c
@@ -6,7 +6,7 @@
 /*   By: joao-pol <joao-pol@student.42porto.co      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/13 02:30:00 by joao-pol          #+#    #+#             */
-/*   Updated: 2024/01/15 13:08:04 by joao-pol         ###   ########.fr       */
+/*   Updated: 2024/02/01 23:34:10 by joao-pol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,3 +20,18 @@ void	ft_swap(int *a, int *b)
 	*a = *b;
 	*b = tmp;
 }
+
+#include <stdio.h>
+
+int main()
+{
+	int *a;
+	int aa = 42;
+
+	a = &aa;
+	int *b;
+	int bb = 21;
+       	b = &bb;
+	ft_swap(a,b);
+	printf("a is now %d and b is now %d", *a, *b);
+}	
