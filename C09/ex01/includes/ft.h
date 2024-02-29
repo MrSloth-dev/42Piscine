9b5423308bd254d8701c806718e086c8d00c68f2
Mr_slothdev <joao8barbosa@gmail.com>
2024-02-29 15:28:56 +0000

    added Shell 00/01 and C09(WIP)

---
+++ C09/ex01/includes/ft.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joao-pol <joao-pol@student.42porto.co      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/29 16:41:49 by joao-pol          #+#    #+#             */
+/*   Updated: 2024/01/30 20:52:45 by joao-pol         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char	*str);
+int		ft_strcmp(char *str1, char *str2);
+#endif
